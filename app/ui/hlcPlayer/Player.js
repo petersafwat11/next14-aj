965e648cbe9070f99189a7f438edeab8e64f1f36
peter safwat <petersafwat640@gmail.com>
2024-08-15 06:02:16 +0300

    testing adding air play

--- app/ui/hlcPlayer/Player.js
+++ app/ui/hlcPlayer/Player.js
@@ -1,48 +1,114 @@
+// "use client";
+// import React from "react";
+// import videojs from "video.js";
+// import "video.js/dist/video-js.css";
+// import classes from "./playerContainer.module.css";
+// export const VideoJS = ({ options, onReady, playerRef }) => {
+//   const divRef = React.useRef(null);
+//   // const playerRef = React.useRef(null);
+//   // const { options, onReady,playerRef } = props;
+
+//   React.useEffect(() => {
+//     // Make sure Video.js player is only initialized once
+//     if (!playerRef.current) {
+//       // The Video.js player needs to be _inside_ the component el for React 18 Strict Mode.
+//       const videoElement = document.createElement("video-js");
+
+//       videoElement.classList.add("vjs-big-play-centered");
+
+//       videoElement.setAttribute("playsinline", "");
+//       videoElement.setAttribute("webkit-playsinline", "true");
+//       divRef.current.appendChild(videoElement);
+
+//       const player = (playerRef.current = videojs(videoElement, options, () => {
+//         videojs.log("player is ready");
+//         player.setAttribute("playsinline", "");
+//         player.setAttribute("webkit-playsinline", "true");
+
+//         onReady && onReady(player);
+//       }));
+
+//       // You could update an existing player in the `else` block here
+//       // on prop change, for example:
+//     } else {
+//       const player = playerRef.current;
+
+//       player.autoplay(options.autoplay);
+//       player.src(options.sources);
+//     }
+//   }, [options, playerRef, divRef, onReady]);
+
+//   // Dispose the Video.js player when the functional component unmounts
+//   React.useEffect(() => {
+//     const player = playerRef.current;
+
+//     return () => {
+//       if (player && !player.isDisposed()) {
+//         player.dispose();
+//         playerRef.current = null;
+//       }
+//     };
+//   }, [playerRef]);
+
+//   return (
+//     <div className={classes["videojs"]} data-vjs-player>
+//       <div className={classes["videojs-2"]} ref={divRef} />
+//     </div>
+//   );
+// };
+
+// export default VideoJS;
 "use client";
 import React from "react";
 import videojs from "video.js";
 import "video.js/dist/video-js.css";
-import airPlay from "videojs-airplay";
-
+import { loadScript } from "./loadScript"; // Utility function to load external scripts
 import classes from "./playerContainer.module.css";
+
 export const VideoJS = ({ options, onReady, playerRef }) => {
   const divRef = React.useRef(null);
-  // const playerRef = React.useRef(null);
-  // const { options, onReady,playerRef } = props;
 
   React.useEffect(() => {
-    // Make sure Video.js player is only initialized once
-    if (!playerRef.current) {
-      // The Video.js player needs to be _inside_ the component el for React 18 Strict Mode.
-      const videoElement = document.createElement("video-js");
+    async function initializePlayer() {
+      await loadScript("/videojs/nuevo.min.js", "nuevo-script");
+      await loadScript("/videojs/videojs.airplay.js", "airplay-script");
 
-      videoElement.classList.add("vjs-big-play-centered", "vjs-airplay-button");
-      videoElement.setAttribute("playsinline", "");
-      videoElement.setAttribute("webkit-playsinline", "true");
-      divRef.current.appendChild(videoElement);
+      if (!playerRef.current) {
+        const videoElement = document.createElement("video-js");
+        videoElement.classList.add("vjs-big-play-centered");
+        videoElement.setAttribute("playsinline", "");
+        videoElement.setAttribute("webkit-playsinline", "true");
+        divRef.current.appendChild(videoElement);
 
-      const player = (playerRef.current = videojs(videoElement, options, () => {
-        videojs.log("player is ready");
-        player.setAttribute("webkit-playsinline", "true");
+        const player = (playerRef.current = videojs(
+          videoElement,
+          options,
+          () => {
+            videojs.log("player is ready");
+            player.setAttribute("playsinline", "");
+            player.setAttribute("webkit-playsinline", "true");
 
-        player.airPlay(); // Initialize the airPlay plugin
+            // Initialize Nuevo plugin
+            player.nuevo({ contextMenu: false });
 
-        onReady && onReady(player);
-      }));
+            // Initialize Airplay plugin
+            player.airplay();
 
-      // You could update an existing player in the `else` block here
-      // on prop change, for example:
-    } else {
-      const player = playerRef.current;
-      player.autoplay(options.autoplay);
-      player.src(options.sources);
+            onReady && onReady(player);
+          }
+        ));
+      } else {
+        const player = playerRef.current;
+        player.autoplay(options.autoplay);
+        player.src(options.sources);
+      }
     }
+
+    initializePlayer();
   }, [options, playerRef, divRef, onReady]);
 
-  // Dispose the Video.js player when the functional component unmounts
   React.useEffect(() => {
     const player = playerRef.current;
-
     return () => {
       if (player && !player.isDisposed()) {
         player.dispose();
