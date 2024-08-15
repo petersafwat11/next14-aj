781c5fd21a4afabd40c8f036c2f99f4c3cf7e060
peter safwat <petersafwat640@gmail.com>
2024-08-15 05:32:02 +0300

    added airplay

--- app/ui/hlcPlayer/Player.js
+++ app/ui/hlcPlayer/Player.js
@@ -2,6 +2,8 @@
 import React from "react";
 import videojs from "video.js";
 import "video.js/dist/video-js.css";
+import airPlay from "videojs-airplay";
+
 import classes from "./playerContainer.module.css";
 export const VideoJS = ({ options, onReady, playerRef }) => {
   const divRef = React.useRef(null);
@@ -14,16 +16,16 @@ export const VideoJS = ({ options, onReady, playerRef }) => {
       // The Video.js player needs to be _inside_ the component el for React 18 Strict Mode.
       const videoElement = document.createElement("video-js");
 
-      videoElement.classList.add("vjs-big-play-centered");
-
+      videoElement.classList.add("vjs-big-play-centered", "vjs-airplay-button");
       videoElement.setAttribute("playsinline", "");
       videoElement.setAttribute("webkit-playsinline", "true");
       divRef.current.appendChild(videoElement);
 
       const player = (playerRef.current = videojs(videoElement, options, () => {
         videojs.log("player is ready");
-        player.setAttribute("playsinline", "");
         player.setAttribute("webkit-playsinline", "true");
+
+        player.airPlay(); // Initialize the airPlay plugin
 
         onReady && onReady(player);
       }));
@@ -32,7 +34,6 @@ export const VideoJS = ({ options, onReady, playerRef }) => {
       // on prop change, for example:
     } else {
       const player = playerRef.current;
-
       player.autoplay(options.autoplay);
       player.src(options.sources);
     }
